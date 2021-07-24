7611b2b5e1fe3de82b16511ec1c07725a5b8b2b1
mtl9-engineer <mohammedtaoufiklahmidi@gmail.com>
2021-07-24 15:15:00 +0100

    Change the return Type Of The call Function to Repository

--- app/src/main/java/com/example/appprojectsfetcher/network/GetRepositoryDataService.java
+++ app/src/main/java/com/example/appprojectsfetcher/network/GetRepositoryDataService.java
@@ -1,13 +1,12 @@
 package com.example.appprojectsfetcher.network;
 
-import com.example.appprojectsfetcher.model.RepositoriesList;
+import com.example.appprojectsfetcher.model.Repository;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface GetRepositoryDataService {
-    @GET("&page={page_num}")
-    Call<RepositoriesList> getRepositoyData (@Query("id") long id , @Path("page_num") int pageNum);
+    @GET("repositories?q=created:>2021-06-23&sort=stars&order=desc")
+    Call<Repository> getRepositoyData (@Query("page") int page);
 }
