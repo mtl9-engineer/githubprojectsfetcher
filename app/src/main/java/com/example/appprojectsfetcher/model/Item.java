ab1064385e2a6c779b35aad8081f5822cd1e1cce
mtl9-engineer <mohammedtaoufiklahmidi@gmail.com>
2021-07-24 15:01:24 +0100

    Change Name of Class Repository to Item & Change from Seriazable to Parcelable

---
+++ app/src/main/java/com/example/appprojectsfetcher/model/Item.java
@@ -0,0 +1,104 @@
+package com.example.appprojectsfetcher.model;
+
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Item implements Parcelable {
+    private Long id;
+    private String name;
+    private String description;
+    private int stargazers_count;
+    private Owner owner;
+
+
+    public Item(Long id, String name, String description, int stargazers_count, Owner owner) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.stargazers_count = stargazers_count;
+        this.owner = owner;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getStargazers_count() {
+        return stargazers_count;
+    }
+
+    public void setStargazers_count(int stargazers_count) {
+        this.stargazers_count = stargazers_count;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public static final Creator<Item> CREATOR = new Creator<Item>() {
+        @Override
+        public Item createFromParcel(Parcel in) {
+            return new Item(in);
+        }
+
+        @Override
+        public Item[] newArray(int size) {
+            return new Item[size];
+        }
+    };
+
+    protected Item(Parcel in) {
+        if (in.readByte() == 0) {
+            id = null;
+        } else {
+            id = in.readLong();
+        }
+        name = in.readString();
+        description = in.readString();
+        stargazers_count = in.readInt();
+        owner = in.readParcelable(Owner.class.getClassLoader());
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        if (id == null) {
+            parcel.writeByte((byte) 0);
+        } else {
+            parcel.writeByte((byte) 1);
+            parcel.writeLong(id);
+        }
+        parcel.writeString(name);
+        parcel.writeString(description);
+        parcel.writeInt(stargazers_count);
+        parcel.writeParcelable(owner,i );
+    }
+}
