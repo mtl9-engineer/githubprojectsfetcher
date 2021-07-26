f59ad082a2fa0ac6a02179d0ee32c267e84f08c5
mtl9-engineer <mohammedtaoufiklahmidi@gmail.com>
2021-07-26 11:26:32 +0100

    Add Loading View Holder

--- app/src/main/java/com/example/appprojectsfetcher/adapter/RepositoryAdapter.java
+++ app/src/main/java/com/example/appprojectsfetcher/adapter/RepositoryAdapter.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -15,35 +16,99 @@ import com.example.appprojectsfetcher.model.Item;
 
 import java.util.List;
 
-public class RepositoryAdapter extends RecyclerView.Adapter<RepositoryAdapter.RepositoryViewHolder> {
-
+public class RepositoryAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static final int ITEM = 0;
+    private static final int LOADING = 1;
 
     private List<Item> dataList;
     private Activity activity;
+    private boolean isLoadingAdded = false;
 
     public RepositoryAdapter(List<Item> dataList , Activity activity) {
         this.dataList = dataList;
         this.activity=activity;
     }
 
-    @Override
-    public RepositoryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View view = layoutInflater.inflate(R.layout.row_repository, parent, false);
-        return new RepositoryViewHolder(view);
+    public List<Item> getDataList() {
+        return dataList;
     }
 
+    public void setDataList(List<Item> dataList) {
+        this.dataList = dataList;
+    }
 
+    public void addLoadingFooter() {
+        isLoadingAdded = true;
+    }
+    public void removeLoadingFooter() {
+        isLoadingAdded = false;
+
+        int position = dataList.size() - 1;
+        Item item = getItem(position);
+        if (item != null) {
+            dataList.remove(position);
+            notifyItemRemoved(position);
+        }
+    }
+    public Item getItem(int position) {
+        return dataList.get(position);
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        RecyclerView.ViewHolder viewHolder = null;
+        switch (viewType) {
+            case ITEM:
+                viewHolder =getViewHolder(parent, layoutInflater);
+                break;
+            case LOADING:
+                View v2 = layoutInflater.inflate(R.layout.progress_bar, parent, false);
+                viewHolder = new LoadingViewHolder(v2);
+                break;
+        }
+        return viewHolder;
+    }
 
     @Override
-    public void onBindViewHolder(RepositoryViewHolder holder, int position) {
-        holder.txt_repository_name.setText(dataList.get(position).getName());
-        holder.txt_repository_score.setText(String.valueOf(dataList.get(position).getStargazers_count()));
-        holder.txt_repository_description.setText(dataList.get(position).getDescription());
-        holder.txt_owner_login.setText(dataList.get(position).getOwner().getLogin());
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
 
-        Glide.with(activity).load(dataList.get(position).getOwner().getAvatar_url()).into(holder.image_owner_avater);
 
+        switch (getItemViewType(position)) {
+            case ITEM:
+                RepositoryViewHolder item = (RepositoryViewHolder) holder;
+                 item.txt_repository_name.setText(dataList.get(position).getName());
+
+                if( dataList.get(position).getStargazers_count() >= 1000){
+                    float stars=dataList.get(position).getStargazers_count() ;
+                    stars = stars / 1000 ;
+                    item.txt_repository_score.setText(String.valueOf(stars)+R.string.k);
+                }
+                else{
+                    item.txt_repository_score.setText(String.valueOf(dataList.get(position).getStargazers_count()));
+                }
+                item.txt_repository_description.setText(dataList.get(position).getDescription());
+                item.txt_owner_login.setText(dataList.get(position).getOwner().getLogin());
+
+                Glide.with(activity).load(dataList.get(position).getOwner().getAvatar_url()).into(item.image_owner_avater);
+                break;
+            case LOADING:
+                break;
+        }
+    }
+
+    @NonNull
+    private RecyclerView.ViewHolder getViewHolder(ViewGroup parent, LayoutInflater inflater) {
+        RecyclerView.ViewHolder viewHolder;
+        View view= inflater.inflate(R.layout.row_repository, parent, false);
+        viewHolder = new RepositoryViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return (position == dataList.size() - 1 && isLoadingAdded) ? LOADING : ITEM;
     }
 
     @Override
@@ -52,9 +117,14 @@ public class RepositoryAdapter extends RecyclerView.Adapter<RepositoryAdapter.Re
         return dataList == null ? 0 :dataList.size() ;
     }
 
-    class RepositoryViewHolder extends RecyclerView.ViewHolder {
 
+    protected class LoadingViewHolder extends RecyclerView.ViewHolder {
 
+        public LoadingViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+    protected class RepositoryViewHolder extends RecyclerView.ViewHolder {
         TextView txt_repository_name;
         TextView txt_repository_score;
         TextView txt_repository_description;
@@ -71,4 +141,7 @@ public class RepositoryAdapter extends RecyclerView.Adapter<RepositoryAdapter.Re
             image_owner_avater = itemView.findViewById(R.id.avatar);
         }
     }
+
+
+
 }
