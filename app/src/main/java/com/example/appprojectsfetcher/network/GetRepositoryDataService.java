33f8b783d628db16eabe51c069998ea73b700512
mtl9-engineer <mohammedtaoufiklahmidi@gmail.com>
2021-07-26 13:49:05 +0100

    Change The link of Query

--- app/src/main/java/com/example/appprojectsfetcher/network/GetRepositoryDataService.java
+++ app/src/main/java/com/example/appprojectsfetcher/network/GetRepositoryDataService.java
@@ -1,12 +1,17 @@
 package com.example.appprojectsfetcher.network;
 
 import com.example.appprojectsfetcher.model.Repository;
+import com.example.appprojectsfetcher.utils.DateConverter;
+
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
+import retrofit2.http.QueryName;
+
 
 public interface GetRepositoryDataService {
-    @GET("repositories?q=created:>2021-06-23&sort=stars&order=desc")
-    Call<Repository> getRepositoyData (@Query("page") int page);
+
+    @GET("repositories?q=sort=stars&order=desc")
+    Call<Repository> getRepositoyData (@Query("page") int page, @QueryName String date );
 }
